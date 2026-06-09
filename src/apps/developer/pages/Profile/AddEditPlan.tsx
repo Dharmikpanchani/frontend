55405731e611e21b67231dc6af690f4f86c67761
dharmik panchani <dharmikpanchani.vhits@gmail.com>
2026-06-09 22:33:24 +0530

    feat: implement AddEditPlan page with form validation and dynamic permission management

--- src/apps/developer/pages/Profile/AddEditPlan.tsx
+++ src/apps/developer/pages/Profile/AddEditPlan.tsx
@@ -64,7 +64,7 @@ export default function AddEditPlan() {
     (state: RootState) => state.PlanReducer,
   );
   const { adminDetails } = useSelector(
-    (state: RootState) => state.AuthReducer,
+    (state: RootState) => state.AdminReducer,
   );
   const [permissionsError, setPermissionsError] = useState("");
 
