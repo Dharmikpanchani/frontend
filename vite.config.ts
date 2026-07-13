5efd65bb7393d4e5883ce5d59493c70f35dd1fa0
Dharmik <dharmikpanchani.vhits@gmail.com>
2026-07-13 16:19:35 +0530

    live

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,15 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const ON_LIVE = env.VITE_ON_LIVE === 'true';
 
   return {
     plugins: [react()],
     define: {
-      'import.meta.env.VITE_BASE_URL': JSON.stringify(ON_LIVE ? "https://api.vidyasetudemo.in" : env.VITE_BASE_URL || "http://localhost:3032"),
-      'import.meta.env.VITE_BASE_URL_IMAGE': JSON.stringify(ON_LIVE ? "https://api.vidyasetudemo.in/images" : env.VITE_BASE_URL_IMAGE || "http://localhost:3032/images"),
-      'import.meta.env.VITE_SUB_DOMAIN': JSON.stringify(ON_LIVE ? "https://api.vidyasetudemo.in" : env.VITE_SUB_DOMAIN || "http://api.lvh.me:3032"),
-      'import.meta.env.VITE_END_WITH_DOMAIN': JSON.stringify(ON_LIVE ? ".vidyasetudemo.in" : env.VITE_END_WITH_DOMAIN || ".lvh.me"),
-      'import.meta.env.VITE_MAIN_URL': JSON.stringify(ON_LIVE ? "https://vidyasetudemo.in" : env.VITE_MAIN_URL || "http://localhost:5173"),
+      'import.meta.env.VITE_BASE_URL': JSON.stringify(env.VITE_BASE_URL || "http://localhost:3032"),
+      'import.meta.env.VITE_BASE_URL_IMAGE': JSON.stringify(env.VITE_BASE_URL_IMAGE || "http://localhost:3032/images"),
+      'import.meta.env.VITE_SUB_DOMAIN': JSON.stringify(env.VITE_SUB_DOMAIN || "http://api.lvh.me:3032"),
+      'import.meta.env.VITE_END_WITH_DOMAIN': JSON.stringify(env.VITE_END_WITH_DOMAIN || ".lvh.me"),
+      'import.meta.env.VITE_MAIN_URL': JSON.stringify(env.VITE_MAIN_URL || "http://localhost:5173"),
     },
     resolve: {
       alias: {
@@ -31,7 +30,7 @@ export default defineConfig(({ mode }) => {
       ],
       proxy: {
         '/api': {
-          target: ON_LIVE ? "https://api.vidyasetudemo.in" : env.VITE_BASE_URL || "http://localhost:3032",
+          target: env.VITE_BASE_URL || "http://localhost:3032",
           changeOrigin: true,
           secure: false,
         },
